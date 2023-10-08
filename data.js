aa3a4d2a198fd16b1b29410fa4e8ac1b344fd8fa
Piotr Stapp <ptrstpp950@gmail.com>
2023-10-08 18:46:10 +0200

    Wersja 1

---
+++ data.js
@@ -0,0 +1,241 @@
+window.dane = {
+    "Legnica": {
+        "nazwaOkregu": "Legnica",
+        "nrOkregu": 1
+    },
+    "Wałbrzych": {
+        "nazwaOkregu": "Wałbrzych",
+        "nrOkregu": 2
+    },
+    "Wrocław": {
+        "nazwaOkregu": "Wrocław",
+        "nrOkregu": 3
+    },
+    "Bydgoszcz": {
+        "nazwaOkregu": "Bydgoszcz",
+        "nrOkregu": 4
+    },
+    "Toruń": {
+        "nazwaOkregu": "Toruń",
+        "nrOkregu": 5
+    },
+    "Lublin": {
+        "nazwaOkregu": "Lublin",
+        "nrOkregu": 6
+    },
+    "Chełm": {
+        "nazwaOkregu": "Chełm",
+        "nrOkregu": 7,
+        "rekomendacjePartii": [
+            "LEWICA",
+            "TD"
+        ]
+    },
+    "Zielona Góra": {
+        "nazwaOkregu": "Zielona Góra",
+        "nrOkregu": 8
+    },
+    "Łódź": {
+        "nazwaOkregu": "Łódź",
+        "nrOkregu": 9,
+        "rekomendacjePartii": [
+            "LEWICA"
+        ]
+    },
+    "Piotrków Trybunalski": {
+        "nazwaOkregu": "Piotrków Trybunalski",
+        "nrOkregu": 10
+    },
+    "Sieradz": {
+        "nazwaOkregu": "Sieradz",
+        "nrOkregu": 11
+    },
+    "Chrzanów": {
+        "nazwaOkregu": "Chrzanów",
+        "nrOkregu": 12,
+        "rekomendacjePartii": [
+            "TD"
+        ]
+    },
+    "Kraków": {
+        "nazwaOkregu": "Kraków",
+        "nrOkregu": 13,
+        "rekomendacjePartii": [
+            "KO",
+            "LEWICA"
+        ]
+    },
+    "Nowy Sącz": {
+        "nazwaOkregu": "Nowy Sącz",
+        "nrOkregu": 14,
+        "rekomendacjePartii": [
+            "TD"
+        ]
+    },
+    "Tarnów": {
+        "nazwaOkregu": "Tarnów",
+        "nrOkregu": 15,
+        "rekomendacjePartii": [
+            "KO"
+        ]
+    },
+    "Płock": {
+        "nazwaOkregu": "Płock",
+        "nrOkregu": 16,
+        "rekomendacjePartii": [
+            "LEWICA"
+        ]
+    },
+    "Radom": {
+        "nazwaOkregu": "Radom",
+        "nrOkregu": 17
+    },
+    "Siedlce": {
+        "nazwaOkregu": "Siedlce",
+        "nrOkregu": 18,
+        "rekomendacjePartii": [
+            ""
+        ]
+    },
+    "Warszawa I": {
+        "nazwaOkregu": "Warszawa I",
+        "nrOkregu": 19,
+        "rekomendacjePartii": [
+            "KO"
+        ]
+    },
+    "Warszawa II": {
+        "nazwaOkregu": "Warszawa II",
+        "nrOkregu": 20,
+        "rekomendacjePartii": [
+            "KO"
+        ]
+    },
+    "Opole": {
+        "nazwaOkregu": "Opole",
+        "nrOkregu": 21
+    },
+    "Krosno": {
+        "nazwaOkregu": "Krosno",
+        "nrOkregu": 22,
+        "rekomendacjePartii": [
+            "TD"
+        ]
+    },
+    "Rzeszów": {
+        "nazwaOkregu": "Rzeszów",
+        "nrOkregu": 23,
+        "rekomendacjePartii": [
+            "KO",
+            "LEWICA"
+        ]
+    },
+    "Białystok": {
+        "nazwaOkregu": "Białystok",
+        "nrOkregu": 24,
+        "rekomendacjePartii": [
+            "KO",
+            "TD"
+        ]
+    },
+    "Gdańsk": {
+        "nazwaOkregu": "Gdańsk",
+        "nrOkregu": 25,
+        "rekomendacjePartii": [
+            ""
+        ]
+    },
+    "Gdynia": {
+        "nazwaOkregu": "Gdynia",
+        "nrOkregu": 26,
+        "rekomendacjePartii": [
+            "LEWICA",
+            "TD"
+        ]
+    },
+    "Bielsko-Biała": {
+        "nazwaOkregu": "Bielsko-Biała",
+        "nrOkregu": 27
+    },
+    "Częstochowa": {
+        "nazwaOkregu": "Częstochowa",
+        "nrOkregu": 28,
+        "rekomendacjePartii": [
+            "TD"
+        ]
+    },
+    "Gliwice": {
+        "nazwaOkregu": "Gliwice",
+        "nrOkregu": 29,
+        "rekomendacjePartii": [
+            "KO"
+        ]
+    },
+    "Rybnik": {
+        "nazwaOkregu": "Rybnik",
+        "nrOkregu": 30
+    },
+    "Katowice": {
+        "nazwaOkregu": "Katowice",
+        "nrOkregu": 31
+    },
+    "Sosnowiec": {
+        "nazwaOkregu": "Sosnowiec",
+        "nrOkregu": 32
+    },
+    "Kielce": {
+        "nazwaOkregu": "Kielce",
+        "nrOkregu": 33
+    },
+    "Elbląg": {
+        "nazwaOkregu": "Elbląg",
+        "nrOkregu": 34,
+        "rekomendacjePartii": [
+            "LEWICA"
+        ]
+    },
+    "Olsztyn": {
+        "nazwaOkregu": "Olsztyn",
+        "nrOkregu": 35
+    },
+    "Kalisz": {
+        "nazwaOkregu": "Kalisz",
+        "nrOkregu": 36
+    },
+    "Konin": {
+        "nazwaOkregu": "Konin",
+        "nrOkregu": 37,
+        "rekomendacjePartii": [
+            "KO"
+        ]
+    },
+    "Piła": {
+        "nazwaOkregu": "Piła",
+        "nrOkregu": 38,
+        "rekomendacjePartii": [
+            "KO"
+        ]
+    },
+    "Poznań": {
+        "nazwaOkregu": "Poznań",
+        "nrOkregu": 39,
+        "rekomendacjePartii": [
+            "KO"
+        ]
+    },
+    "Koszalin": {
+        "nazwaOkregu": "Koszalin",
+        "nrOkregu": 40,
+        "rekomendacjePartii": [
+            ""
+        ]
+    },
+    "Szczecin": {
+        "nazwaOkregu": "Szczecin",
+        "nrOkregu": 41,
+        "rekomendacjePartii": [
+            ""
+        ]
+    }
+}
+
