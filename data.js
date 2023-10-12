06274eae150aa283c2d88598ea6a17e48efd14d1
Piotr Stapp <ptrstpp950@gmail.com>
2023-10-12 23:36:29 +0200

    Added recomendation zielona góra

--- data.js
+++ data.js
@@ -349,7 +349,7 @@ window.rekomendacje = {
     "3": "Wyborcy KO powinni głosować w tym okręgu, aby być pewnym zdobycia szóstego mandatu",
     "6": "Wszystkie partie opozycyjne mają szanse na kolejny mandat, W walce z PiS o ostatni mandat potrzebna jest mobilizacjia wszystkich wyborców opozycji.",
     "7": "KO ze stabilnym poparciem na dwa mandaty, Trzecia Droga ze stabilnym poparciem na jeden mandat. Lewica z umiarkowanymi szansami na pierwszy mandat. Wsparcie jej potencjalnie również wyborcami Lewicy z Krosna może zwiększyć szanse na mandat. Stąd rekomendacja wsparcia Lewicy.",
-    "8": "Dla scenariusza jak tu, że 80% układ mandatów między opozycją się nie zmieni, ja zostawiłbym bez rekomendacji",
+    "8": "Żeby zdobyć 1 mandat więcej trzeba się skupić na dwóch z trzech ugrupowaniach. Najsilniejsze wydają się KO i TD.",
     "10": "KO ze stabilnym poparciem na dwa mandaty, Lewica walczy o pierwszy mandat przy wsparciu wyborców z Radomia. Trzecia Droga może potrzebować wsparcia dla obrony pierwszego mandatu.",
     "11": "Partie opozycyjne mają potencjał wzrostowy zależny od mobilizacji/. Najbliżej drugiego mandatu jest Trzecia Droga.",
     "13": "Wyborcy Trzeciej Drogi mają stabilne poparcie na pierwszy mandat i mogą wesprzeć turystyką wyborczą walkę o mandat w Chrzanowie. Wyborcy Lewicy powinni głosować w Krakowie i walczyć o dwa mandaty. Wyborcy KO mogą wykorzystać turystykę wyborczą i wesprzeć walkę o kolejny mandat w Tarnowie.",
